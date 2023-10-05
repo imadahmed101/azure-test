1ec3c3b334a05c10b222011396b342875fa8d9ec
Imad Ahmed <imadahmed101@gmail.com>
2023-10-05 13:12:49 -0400

    updated files

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-})
 
-app.listen(port, () => {
-    console.log('server running')
-})
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//     res.send('Hello world')
+// })
+
+// app.listen(port, () => {
+//     console.log('server running')
+// })
