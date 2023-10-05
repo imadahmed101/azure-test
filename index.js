5990ae4588f616f1ffd70250c5bce096f6a59853
Imad Ahmed <imadahmed101@gmail.com>
2023-10-05 16:20:36 -0400

    added cors to index

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const app = express()
 app.use(express.json())
 // const env = require('dotenv/config')
 // const mongoose = require('mongoose')
-// const cors = require('cors')
+const cors = require('cors')
 
 const studentRoute = require('./routes/student')
 // const postRoute = require('./routes/post')
@@ -68,7 +68,7 @@ const studentRoute = require('./routes/student')
 const PORT = process.env.PORT || 80
 // const DBCONNECTION = process.env.DBCONNECTION
 
-// app.use(cors())
+app.use(cors())
 app.use('/', (studentRoute))
 // app.use('/post', (postRoute))
 // app.use('/comment', (commentRoute))
